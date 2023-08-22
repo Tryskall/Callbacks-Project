2c4d54d561e09372a19f18142af01d204e7c4bfd
Tryskall <119624233+Tryskall@users.noreply.github.com>
2023-08-22 09:50:04 -0700

    first commit

---
+++ problems copy/09-chain-map.js
@@ -0,0 +1,53 @@
+/*******************************************************************************
+Write a function `chainMap` that accepts a value and any number of callbacks as
+arguments. The function should return the final result of passing the value through
+all of the given callbacks. In other words, if three callbacks are given then:
+
+- the value is given to the first callback
+- the result of the first callback is given to the second callback
+- the result of the second callback is given to the third callback
+- the result of the third callback is the final result
+
+Examples:
+
+let add5 = function(n) {
+    return n + 5;
+};
+
+let half = function(n) {
+    return n / 2;
+};
+
+let square = function(n) {
+    return n * n;
+};
+
+console.log(chainMap(25, add5));                // 30
+console.log(chainMap(25, add5, half));          // 15
+console.log(chainMap(25, add5, half, square));  // 225
+console.log(chainMap(4, square, half));         // 8
+console.log(chainMap(4, half, square));         // 4
+*******************************************************************************/
+
+// let chainMap = function(value, cb1, cb2, cb3) {
+//     let totalValue = value;
+
+//         if (cb2 === undefined && cb3 === undefined) {
+//             totalValue = cb1(totalValue);
+//             // console.log(totalValue);
+//         } else if (cb3 === undefined) {
+//             totalValue = cb2(cb1(totalValue));
+//             // console.log(totalValue);
+//         } else {
+//             totalValue = cb3(cb2(cb1(totalValue)));
+//             // console.log(totalValue);
+//         }
+//     return totalValue;
+// };
+
+let chainMap = (value, ...cbs) => {
+    return  cbs.reduce((accum, cb) => cb(accum), value);
+};
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = chainMap;
